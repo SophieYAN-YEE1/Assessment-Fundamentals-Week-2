72101267e0281c0eec9d7ac032c6456146520ac1
SophieYAN-YEE1 <sophieyylin123@gmail.com>
2026-06-26 14:35:34 +0100

    Completed Task 1

--- Level-1/task_one.py
+++ Level-1/task_one.py
@@ -1,6 +1,49 @@
 from datetime import date
 
 
+class Trainee:
+    """Trainee Class"""
+
+    def __init__(self, name: str, email: str, date_of_birth: date) -> None:
+        """Initialising Trainee"""
+        self.name = name
+        self.email = email
+        self.date_of_birth = date_of_birth
+        self.assessments = []
+
+    def get_age(self) -> int:
+        """Returns the age of the trainee"""
+        today = date.today()
+        age = today.year - self.date_of_birth.year
+        if today.month <= self.date_of_birth.month:
+            if today.day < self.date_of_birth.day:
+                age -= 1
+        return age
+
+    def add_assessment(self, assessment: Assessment) -> None:
+        """Adds an Assessment to the trainee's list of assessments"""
+        self.assessments.append(assessment)
+
+    def get_assessment(self, name: str) -> Assessment | None:
+        """Returns the Assessment object based on the name, returns non if not found"""
+        for assessment in self.assessments:
+            if assessment.name == name:
+                return assessment
+        return None
+
+
+class Assessment:
+    """Assessment Class"""
+
+    def __init__(self, name: str, type: str, score: float) -> None:
+        """Initialising Assessment"""
+        self.name = name
+        self.type = type.lower()
+        self.score = score
+
+        if self.type not in ["multiple-choice", "technical", "presentation"]:
+            raise ValueError("Not a valid assessment type")
+
 
 if __name__ == "__main__":
     trainee = Trainee("Sigma", "trainee@sigmalabs.co.uk", date(1990, 1, 1))
